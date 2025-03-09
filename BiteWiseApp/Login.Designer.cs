30469e3f9774ca21d540bd0f6bd9ac05899bad40
Sannteen <143964654+Sannteen@users.noreply.github.com>
2025-03-09 14:33:35 -0500

    Test Push

--- BiteWiseApp/Login.Designer.cs
+++ BiteWiseApp/Login.Designer.cs
@@ -35,6 +35,7 @@
             this.LoginBtn = new System.Windows.Forms.Button();
             this.SignUpLb = new System.Windows.Forms.Label();
             this.Signup = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -101,11 +102,21 @@
             this.Signup.Text = "Sign Up";
             this.Signup.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Signup_LinkClicked);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(144, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(613, 316);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.Signup);
             this.Controls.Add(this.SignUpLb);
             this.Controls.Add(this.LoginBtn);
@@ -130,6 +141,7 @@
         private System.Windows.Forms.Button LoginBtn;
         private System.Windows.Forms.Label SignUpLb;
         private System.Windows.Forms.LinkLabel Signup;
+        private System.Windows.Forms.Label label3;
     }
 }
 
