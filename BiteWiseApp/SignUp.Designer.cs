c55f33827eec258eb1c2cbaf9f957c3e92d25e71
Sannteen <143964654+Sannteen@users.noreply.github.com>
2025-03-16 19:43:39 -0500

    User Registration form

--- BiteWiseApp/SignUp.Designer.cs
+++ BiteWiseApp/SignUp.Designer.cs
@@ -28,12 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Savebtn = new System.Windows.Forms.Button();
+            this.NameTB = new System.Windows.Forms.TextBox();
+            this.NameLbl = new System.Windows.Forms.Label();
+            this.EmailLbl = new System.Windows.Forms.Label();
+            this.EmailTB = new System.Windows.Forms.TextBox();
+            this.AgeTB = new System.Windows.Forms.TextBox();
+            this.AgeLbl = new System.Windows.Forms.Label();
+            this.GenderLbl = new System.Windows.Forms.Label();
+            this.GenderCB = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.WeightLbl = new System.Windows.Forms.Label();
+            this.HeightLbl = new System.Windows.Forms.Label();
+            this.HeightTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GoalLbl = new System.Windows.Forms.Label();
+            this.ActivityLvlTB = new System.Windows.Forms.ComboBox();
+            this.GoalTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Savebtn
+            // 
+            this.Savebtn.Location = new System.Drawing.Point(390, 479);
+            this.Savebtn.Name = "Savebtn";
+            this.Savebtn.Size = new System.Drawing.Size(104, 44);
+            this.Savebtn.TabIndex = 0;
+            this.Savebtn.Text = "Save";
+            this.Savebtn.UseVisualStyleBackColor = true;
+            // 
+            // NameTB
+            // 
+            this.NameTB.Location = new System.Drawing.Point(165, 196);
+            this.NameTB.Name = "NameTB";
+            this.NameTB.Size = new System.Drawing.Size(130, 20);
+            this.NameTB.TabIndex = 1;
+            // 
+            // NameLbl
+            // 
+            this.NameLbl.AutoSize = true;
+            this.NameLbl.Location = new System.Drawing.Point(104, 203);
+            this.NameLbl.Name = "NameLbl";
+            this.NameLbl.Size = new System.Drawing.Size(35, 13);
+            this.NameLbl.TabIndex = 2;
+            this.NameLbl.Text = "Name";
+            // 
+            // EmailLbl
+            // 
+            this.EmailLbl.AutoSize = true;
+            this.EmailLbl.Location = new System.Drawing.Point(352, 198);
+            this.EmailLbl.Name = "EmailLbl";
+            this.EmailLbl.Size = new System.Drawing.Size(32, 13);
+            this.EmailLbl.TabIndex = 3;
+            this.EmailLbl.Text = "Email";
+            // 
+            // EmailTB
+            // 
+            this.EmailTB.Location = new System.Drawing.Point(421, 191);
+            this.EmailTB.Name = "EmailTB";
+            this.EmailTB.Size = new System.Drawing.Size(134, 20);
+            this.EmailTB.TabIndex = 4;
+            // 
+            // AgeTB
+            // 
+            this.AgeTB.Location = new System.Drawing.Point(639, 191);
+            this.AgeTB.Name = "AgeTB";
+            this.AgeTB.Size = new System.Drawing.Size(141, 20);
+            this.AgeTB.TabIndex = 5;
+            // 
+            // AgeLbl
+            // 
+            this.AgeLbl.AutoSize = true;
+            this.AgeLbl.Location = new System.Drawing.Point(590, 196);
+            this.AgeLbl.Name = "AgeLbl";
+            this.AgeLbl.Size = new System.Drawing.Size(26, 13);
+            this.AgeLbl.TabIndex = 6;
+            this.AgeLbl.Text = "Age";
+            // 
+            // GenderLbl
+            // 
+            this.GenderLbl.AutoSize = true;
+            this.GenderLbl.Location = new System.Drawing.Point(104, 261);
+            this.GenderLbl.Name = "GenderLbl";
+            this.GenderLbl.Size = new System.Drawing.Size(42, 13);
+            this.GenderLbl.TabIndex = 7;
+            this.GenderLbl.Text = "Gender";
+            // 
+            // GenderCB
+            // 
+            this.GenderCB.FormattingEnabled = true;
+            this.GenderCB.Items.AddRange(new object[] {
+            "",
+            "Female",
+            "Male",
+            "Others"});
+            this.GenderCB.Location = new System.Drawing.Point(165, 258);
+            this.GenderCB.Name = "GenderCB";
+            this.GenderCB.Size = new System.Drawing.Size(130, 21);
+            this.GenderCB.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(421, 254);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // WeightLbl
+            // 
+            this.WeightLbl.AutoSize = true;
+            this.WeightLbl.Location = new System.Drawing.Point(352, 261);
+            this.WeightLbl.Name = "WeightLbl";
+            this.WeightLbl.Size = new System.Drawing.Size(41, 13);
+            this.WeightLbl.TabIndex = 2;
+            this.WeightLbl.Text = "Weight";
+            // 
+            // HeightLbl
+            // 
+            this.HeightLbl.AutoSize = true;
+            this.HeightLbl.Location = new System.Drawing.Point(590, 260);
+            this.HeightLbl.Name = "HeightLbl";
+            this.HeightLbl.Size = new System.Drawing.Size(38, 13);
+            this.HeightLbl.TabIndex = 3;
+            this.HeightLbl.Text = "Height";
+            // 
+            // HeightTB
+            // 
+            this.HeightTB.Location = new System.Drawing.Point(639, 254);
+            this.HeightTB.Name = "HeightTB";
+            this.HeightTB.Size = new System.Drawing.Size(141, 20);
+            this.HeightTB.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(352, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Activity Lvl";
+            // 
+            // GoalLbl
+            // 
+            this.GoalLbl.AutoSize = true;
+            this.GoalLbl.Location = new System.Drawing.Point(104, 336);
+            this.GoalLbl.Name = "GoalLbl";
+            this.GoalLbl.Size = new System.Drawing.Size(29, 13);
+            this.GoalLbl.TabIndex = 7;
+            this.GoalLbl.Text = "Goal";
+            // 
+            // ActivityLvlTB
+            // 
+            this.ActivityLvlTB.FormattingEnabled = true;
+            this.ActivityLvlTB.Items.AddRange(new object[] {
+            "",
+            "High",
+            "Medium",
+            "Low"});
+            this.ActivityLvlTB.Location = new System.Drawing.Point(421, 328);
+            this.ActivityLvlTB.Name = "ActivityLvlTB";
+            this.ActivityLvlTB.Size = new System.Drawing.Size(139, 21);
+            this.ActivityLvlTB.TabIndex = 8;
+            // 
+            // GoalTB
+            // 
+            this.GoalTB.Location = new System.Drawing.Point(165, 329);
+            this.GoalTB.Name = "GoalTB";
+            this.GoalTB.Size = new System.Drawing.Size(130, 20);
+            this.GoalTB.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(281, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 42);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "User Registration";
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(885, 541);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ActivityLvlTB);
+            this.Controls.Add(this.GoalLbl);
+            this.Controls.Add(this.GenderCB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GenderLbl);
+            this.Controls.Add(this.AgeLbl);
+            this.Controls.Add(this.GoalTB);
+            this.Controls.Add(this.AgeTB);
+            this.Controls.Add(this.HeightTB);
+            this.Controls.Add(this.HeightLbl);
+            this.Controls.Add(this.EmailTB);
+            this.Controls.Add(this.EmailLbl);
+            this.Controls.Add(this.WeightLbl);
+            this.Controls.Add(this.NameLbl);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.NameTB);
+            this.Controls.Add(this.Savebtn);
+            this.Name = "SignUp";
             this.Text = "SignUp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Savebtn;
+        private System.Windows.Forms.TextBox NameTB;
+        private System.Windows.Forms.Label NameLbl;
+        private System.Windows.Forms.Label EmailLbl;
+        private System.Windows.Forms.TextBox EmailTB;
+        private System.Windows.Forms.TextBox AgeTB;
+        private System.Windows.Forms.Label AgeLbl;
+        private System.Windows.Forms.Label GenderLbl;
+        private System.Windows.Forms.ComboBox GenderCB;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label WeightLbl;
+        private System.Windows.Forms.Label HeightLbl;
+        private System.Windows.Forms.TextBox HeightTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label GoalLbl;
+        private System.Windows.Forms.ComboBox ActivityLvlTB;
+        private System.Windows.Forms.TextBox GoalTB;
+        private System.Windows.Forms.Label label2;
     }
 }
