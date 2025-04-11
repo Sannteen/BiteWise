152699875c5c1ab015206d252ebc79dd23eb7683
Ladybug90 <famousrunner@gmail.com>
2025-04-10 22:35:33 -0500

    model1 entered

--- BiteWiseApp/SummaryForm.Designer.cs
+++ BiteWiseApp/SummaryForm.Designer.cs
@@ -62,31 +62,28 @@
             this.DailySumarylabel.AutoSize = true;
             this.DailySumarylabel.Font = new System.Drawing.Font("Bookman Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DailySumarylabel.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.DailySumarylabel.Location = new System.Drawing.Point(8, 6);
-            this.DailySumarylabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.DailySumarylabel.Location = new System.Drawing.Point(12, 9);
             this.DailySumarylabel.Name = "DailySumarylabel";
-            this.DailySumarylabel.Size = new System.Drawing.Size(286, 38);
+            this.DailySumarylabel.Size = new System.Drawing.Size(424, 56);
             this.DailySumarylabel.TabIndex = 14;
             this.DailySumarylabel.Text = "Daily Summary ";
             // 
             // DatePickfordailysumry
             // 
             this.DatePickfordailysumry.AllowDrop = true;
-            this.DatePickfordailysumry.Location = new System.Drawing.Point(49, 63);
-            this.DatePickfordailysumry.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DatePickfordailysumry.Location = new System.Drawing.Point(74, 97);
             this.DatePickfordailysumry.MaxDate = new System.DateTime(2025, 3, 20, 0, 0, 0, 0);
             this.DatePickfordailysumry.Name = "DatePickfordailysumry";
-            this.DatePickfordailysumry.Size = new System.Drawing.Size(199, 20);
+            this.DatePickfordailysumry.Size = new System.Drawing.Size(296, 26);
             this.DatePickfordailysumry.TabIndex = 15;
             this.DatePickfordailysumry.Value = new System.DateTime(2025, 3, 20, 0, 0, 0, 0);
             // 
             // DateLabel
             // 
             this.DateLabel.AutoSize = true;
-            this.DateLabel.Location = new System.Drawing.Point(15, 63);
-            this.DateLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.DateLabel.Location = new System.Drawing.Point(22, 97);
             this.DateLabel.Name = "DateLabel";
-            this.DateLabel.Size = new System.Drawing.Size(30, 13);
+            this.DateLabel.Size = new System.Drawing.Size(44, 20);
             this.DateLabel.TabIndex = 16;
             this.DateLabel.Text = "Date";
             // 
@@ -94,10 +91,9 @@
             // 
             this.foodloglabel.AutoSize = true;
             this.foodloglabel.Font = new System.Drawing.Font("Segoe UI Black", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.foodloglabel.Location = new System.Drawing.Point(45, 103);
-            this.foodloglabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.foodloglabel.Location = new System.Drawing.Point(69, 171);
             this.foodloglabel.Name = "foodloglabel";
-            this.foodloglabel.Size = new System.Drawing.Size(79, 20);
+            this.foodloglabel.Size = new System.Drawing.Size(113, 30);
             this.foodloglabel.TabIndex = 18;
             this.foodloglabel.Text = "Food Log";
             this.foodloglabel.Click += new System.EventHandler(this.foodloglabel_Click);
@@ -106,10 +102,9 @@
             // 
             this.WoutoutLogLabel.AutoSize = true;
             this.WoutoutLogLabel.Font = new System.Drawing.Font("Segoe UI Black", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.WoutoutLogLabel.Location = new System.Drawing.Point(199, 111);
-            this.WoutoutLogLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.WoutoutLogLabel.Location = new System.Drawing.Point(298, 171);
             this.WoutoutLogLabel.Name = "WoutoutLogLabel";
-            this.WoutoutLogLabel.Size = new System.Drawing.Size(108, 20);
+            this.WoutoutLogLabel.Size = new System.Drawing.Size(156, 30);
             this.WoutoutLogLabel.TabIndex = 21;
             this.WoutoutLogLabel.Text = "Workout Log";
             // 
@@ -119,33 +114,30 @@
             this.totalCalConsulabel.BackColor = System.Drawing.SystemColors.Control;
             this.totalCalConsulabel.Font = new System.Drawing.Font("Segoe UI Black", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.totalCalConsulabel.ForeColor = System.Drawing.Color.Black;
-            this.totalCalConsulabel.Location = new System.Drawing.Point(371, 42);
-            this.totalCalConsulabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.totalCalConsulabel.Location = new System.Drawing.Point(556, 65);
             this.totalCalConsulabel.Name = "totalCalConsulabel";
-            this.totalCalConsulabel.Size = new System.Drawing.Size(191, 20);
+            this.totalCalConsulabel.Size = new System.Drawing.Size(282, 30);
             this.totalCalConsulabel.TabIndex = 24;
             this.totalCalConsulabel.Text = "Total Calories Consumed";
             // 
             // foodlogdataGridView
             // 
             this.foodlogdataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.foodlogdataGridView.Location = new System.Drawing.Point(15, 133);
-            this.foodlogdataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.foodlogdataGridView.Location = new System.Drawing.Point(22, 205);
             this.foodlogdataGridView.Name = "foodlogdataGridView";
             this.foodlogdataGridView.RowHeadersWidth = 62;
             this.foodlogdataGridView.RowTemplate.Height = 28;
-            this.foodlogdataGridView.Size = new System.Drawing.Size(157, 141);
+            this.foodlogdataGridView.Size = new System.Drawing.Size(236, 217);
             this.foodlogdataGridView.TabIndex = 26;
             // 
             // workoutlogdatagridView2
             // 
             this.workoutlogdatagridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.workoutlogdatagridView2.Location = new System.Drawing.Point(191, 133);
-            this.workoutlogdatagridView2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.workoutlogdatagridView2.Location = new System.Drawing.Point(286, 205);
             this.workoutlogdatagridView2.Name = "workoutlogdatagridView2";
             this.workoutlogdatagridView2.RowHeadersWidth = 62;
             this.workoutlogdatagridView2.RowTemplate.Height = 28;
-            this.workoutlogdatagridView2.Size = new System.Drawing.Size(156, 141);
+            this.workoutlogdatagridView2.Size = new System.Drawing.Size(234, 217);
             this.workoutlogdatagridView2.TabIndex = 30;
             // 
             // tableLayoutPanel1
@@ -153,14 +145,13 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 94.31818F));
             this.tableLayoutPanel1.Controls.Add(this.tltCaloriesconsumedchart1, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(366, 63);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(549, 97);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.36201F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.637982F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 188F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(232, 380);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 289F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(348, 585);
             this.tableLayoutPanel1.TabIndex = 31;
             // 
             // tltCaloriesconsumedchart1
@@ -169,15 +160,14 @@
             this.tltCaloriesconsumedchart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.tltCaloriesconsumedchart1.Legends.Add(legend1);
-            this.tltCaloriesconsumedchart1.Location = new System.Drawing.Point(2, 2);
-            this.tltCaloriesconsumedchart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tltCaloriesconsumedchart1.Location = new System.Drawing.Point(3, 3);
             this.tltCaloriesconsumedchart1.Name = "tltCaloriesconsumedchart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.tltCaloriesconsumedchart1.Series.Add(series1);
-            this.tltCaloriesconsumedchart1.Size = new System.Drawing.Size(228, 177);
+            this.tltCaloriesconsumedchart1.Size = new System.Drawing.Size(342, 272);
             this.tltCaloriesconsumedchart1.TabIndex = 26;
             this.tltCaloriesconsumedchart1.Text = "chart1";
             // 
@@ -186,12 +176,11 @@
             this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.Controls.Add(this.TotalCalBurned, 0, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(18, 289);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(27, 445);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(329, 153);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(494, 235);
             this.tableLayoutPanel2.TabIndex = 32;
             this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
             // 
@@ -201,8 +190,7 @@
             this.TotalCalBurned.ChartAreas.Add(chartArea2);
             legend2.Name = "Legend1";
             this.TotalCalBurned.Legends.Add(legend2);
-            this.TotalCalBurned.Location = new System.Drawing.Point(2, 2);
-            this.TotalCalBurned.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TotalCalBurned.Location = new System.Drawing.Point(3, 3);
             this.TotalCalBurned.Name = "TotalCalBurned";
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
@@ -221,7 +209,7 @@
             this.TotalCalBurned.Series.Add(series2);
             this.TotalCalBurned.Series.Add(series3);
             this.TotalCalBurned.Series.Add(series4);
-            this.TotalCalBurned.Size = new System.Drawing.Size(325, 149);
+            this.TotalCalBurned.Size = new System.Drawing.Size(488, 229);
             this.TotalCalBurned.TabIndex = 7;
             this.TotalCalBurned.Text = "Body Measurement History ";
             title1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -231,9 +219,9 @@
             // 
             // SummaryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(606, 457);
+            this.ClientSize = new System.Drawing.Size(909, 703);
             this.Controls.Add(this.WoutoutLogLabel);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.tableLayoutPanel1);
@@ -244,7 +232,6 @@
             this.Controls.Add(this.DateLabel);
             this.Controls.Add(this.DatePickfordailysumry);
             this.Controls.Add(this.DailySumarylabel);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SummaryForm";
             this.Text = "SummaryForm";
             this.Load += new System.EventHandler(this.SummaryForm_Load);
--- BiteWiseApp/UserProfile.cs
+++ BiteWiseApp/UserProfile.cs
@@ -11,115 +11,59 @@ using System.Windows.Forms;
 namespace BiteWiseApp
 {
     public partial class UserProfileForm: Form
+
     {
         public UserProfileForm()
         {
             InitializeComponent();
         }
 
-        string ImageLocation;
-
-
-
-
-
-        private void profilepictureBox1_Click(object sender, EventArgs e)
+        private void SaveChangesButton_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Passwordlabel_Click(object sender, EventArgs e)
-        {
-
+            MessageBox.Show("Changes save successfully");
         }
 
         private void PasswordtextBox_TextChanged(object sender, EventArgs e)
         {
-
+            PasswordtextBox.PasswordChar = '*';
         }
 
-        private void Emaillabel_Click(object sender, EventArgs e)
+        private void PasswordcheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
-        }
-
-        private void EmailtextBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void nametextBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void NameLabel_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Usernamelabel_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void profilepicture_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void savechangesbutn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void AgetxtBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void HeighttxtBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void DCaloricTgetLabel_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void PicUploadbutton_Click(object sender, EventArgs e)
-        {
-             try
+            if (PasswordcheckBox.Checked)
             {
-                //Select & filter Image by file Extension.
-                OpenFileDialog Ofd = new OpenFileDialog();
-                Ofd.Filter = "JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*gif |PNG Files (*.png)|*png |All Files (*.*)|*.*";
-                Ofd.Title = "Profile Picture";
+                PasswordtextBox.PasswordChar = '\0';
+            }
+            else
+            {
+                PasswordtextBox.PasswordChar = '*';
+            }
+        }
 
+        private void UploadImageButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Title = "Select Profile Picture";
+                dialog.Filter = "JPG files (*.jpg)|*.jpg|PNG files (*.png)|*.png|All files (*.*)|*.*";
 
-                if (Ofd.ShowDialog() == DialogResult.OK)
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    ImageLocation = Ofd.FileName.ToString();
-                    profilepicture.ImageLocation = ImageLocation;
+                    string imageLocation = dialog.FileName;
+                    ProfilePictureBox.ImageLocation = imageLocation;
+
+                    //SaveImagePathToDatabase(imageLocation); // Save to DB
                 }
-
             }
-
-            catch (Exception EX)
+            catch (Exception ex)
             {
-                MessageBox.Show(EX.Message, "Profile Picture" , MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("An error occurred: " + ex.Message);
             }
+
+
         }
+                    
+                   
     }
 }
