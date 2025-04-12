e998e712136c549301f099b01a3e955153e7be99
Kemar Lemonious <lemoniousk@gmail.com>
2025-04-12 13:50:43 -0500

    Updated and connected Recipes Form to Database. Corrected item names.

--- BiteWiseApp/Recipes.Designer.cs
+++ BiteWiseApp/Recipes.Designer.cs
@@ -28,70 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.Label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.JamRecipeSearchTxtBx = new System.Windows.Forms.TextBox();
+            this.JamRecipeSearchLabel = new System.Windows.Forms.Label();
+            this.RecipeDisplayTxtBx = new System.Windows.Forms.TextBox();
+            this.RecipeOfDayLabel = new System.Windows.Forms.Label();
+            this.BitwiseLogoRecipes = new System.Windows.Forms.Label();
             this.GetRecipeBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // JamRecipeSearchTxtBx
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.JamRecipeSearchTxtBx.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(41, 40);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(449, 20);
-            this.textBox1.TabIndex = 0;
+            this.JamRecipeSearchTxtBx.Location = new System.Drawing.Point(62, 62);
+            this.JamRecipeSearchTxtBx.Name = "JamRecipeSearchTxtBx";
+            this.JamRecipeSearchTxtBx.Size = new System.Drawing.Size(672, 26);
+            this.JamRecipeSearchTxtBx.TabIndex = 0;
             // 
-            // label1
+            // JamRecipeSearchLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 21);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Search Jamaican Recipes";
+            this.JamRecipeSearchLabel.AutoSize = true;
+            this.JamRecipeSearchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.JamRecipeSearchLabel.Location = new System.Drawing.Point(63, 32);
+            this.JamRecipeSearchLabel.Name = "JamRecipeSearchLabel";
+            this.JamRecipeSearchLabel.Size = new System.Drawing.Size(315, 29);
+            this.JamRecipeSearchLabel.TabIndex = 1;
+            this.JamRecipeSearchLabel.Text = "Search Jamaican Recipes";
             // 
-            // textBox2
+            // RecipeDisplayTxtBx
             // 
-            this.textBox2.Location = new System.Drawing.Point(41, 107);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(449, 98);
-            this.textBox2.TabIndex = 2;
+            this.RecipeDisplayTxtBx.Location = new System.Drawing.Point(62, 165);
+            this.RecipeDisplayTxtBx.Multiline = true;
+            this.RecipeDisplayTxtBx.Name = "RecipeDisplayTxtBx";
+            this.RecipeDisplayTxtBx.Size = new System.Drawing.Size(672, 149);
+            this.RecipeDisplayTxtBx.TabIndex = 2;
             // 
-            // Label2
+            // RecipeOfDayLabel
             // 
-            this.Label2.AutoSize = true;
-            this.Label2.Location = new System.Drawing.Point(42, 81);
-            this.Label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.Label2.Name = "Label2";
-            this.Label2.Size = new System.Drawing.Size(121, 13);
-            this.Label2.TabIndex = 3;
-            this.Label2.Text = "Your Recipe of the Day!";
+            this.RecipeOfDayLabel.AutoSize = true;
+            this.RecipeOfDayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RecipeOfDayLabel.Location = new System.Drawing.Point(63, 125);
+            this.RecipeOfDayLabel.Name = "RecipeOfDayLabel";
+            this.RecipeOfDayLabel.Size = new System.Drawing.Size(288, 29);
+            this.RecipeOfDayLabel.TabIndex = 3;
+            this.RecipeOfDayLabel.Text = "Your Recipe of the Day!";
             // 
-            // label3
+            // BitwiseLogoRecipes
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Juice ITC", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(303, 216);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(187, 72);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "BiteWise";
+            this.BitwiseLogoRecipes.AutoSize = true;
+            this.BitwiseLogoRecipes.Font = new System.Drawing.Font("Juice ITC", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BitwiseLogoRecipes.ForeColor = System.Drawing.Color.Red;
+            this.BitwiseLogoRecipes.Location = new System.Drawing.Point(454, 332);
+            this.BitwiseLogoRecipes.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.BitwiseLogoRecipes.Name = "BitwiseLogoRecipes";
+            this.BitwiseLogoRecipes.Size = new System.Drawing.Size(279, 109);
+            this.BitwiseLogoRecipes.TabIndex = 8;
+            this.BitwiseLogoRecipes.Text = "BiteWise";
             // 
             // GetRecipeBtn
             // 
-            this.GetRecipeBtn.Location = new System.Drawing.Point(45, 233);
-            this.GetRecipeBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.GetRecipeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GetRecipeBtn.Location = new System.Drawing.Point(68, 358);
             this.GetRecipeBtn.Name = "GetRecipeBtn";
-            this.GetRecipeBtn.Size = new System.Drawing.Size(167, 44);
+            this.GetRecipeBtn.Size = new System.Drawing.Size(250, 68);
             this.GetRecipeBtn.TabIndex = 9;
             this.GetRecipeBtn.Text = "Get New Recipe";
             this.GetRecipeBtn.UseVisualStyleBackColor = true;
@@ -99,18 +98,18 @@
             // 
             // Recipes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.ClientSize = new System.Drawing.Size(800, 449);
             this.Controls.Add(this.GetRecipeBtn);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.Label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.BitwiseLogoRecipes);
+            this.Controls.Add(this.RecipeOfDayLabel);
+            this.Controls.Add(this.RecipeDisplayTxtBx);
+            this.Controls.Add(this.JamRecipeSearchLabel);
+            this.Controls.Add(this.JamRecipeSearchTxtBx);
             this.Name = "Recipes";
             this.Text = "Recipes";
+            this.Load += new System.EventHandler(this.Recipes_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,11 +117,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label Label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox JamRecipeSearchTxtBx;
+        private System.Windows.Forms.Label JamRecipeSearchLabel;
+        private System.Windows.Forms.TextBox RecipeDisplayTxtBx;
+        private System.Windows.Forms.Label RecipeOfDayLabel;
+        private System.Windows.Forms.Label BitwiseLogoRecipes;
         private System.Windows.Forms.Button GetRecipeBtn;
     }
 }
--- BiteWiseApp/Recipes.cs
+++ BiteWiseApp/Recipes.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,6 +20,56 @@ namespace BiteWiseApp
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            // Retrieve the connection string from App.config
+            string connectionString = ConfigurationManager.ConnectionStrings["BiteWiseDB_Connect"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // SQL query to fetch a random recipe from the JamaicanRecipes table
+                    string query = @"
+                        SELECT TOP 1 
+                            recipename, 
+                            description, 
+                            calories_Per_Serving, 
+                            ingredients, 
+                            instructions 
+                        FROM JamaicanRecipes 
+                        ORDER BY NEWID()";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // Format and display the recipe details in the RecipeDisplayTxtBx
+                                RecipeDisplayTxtBx.Text =
+                                    $"Recipe Name: {reader["recipename"]}\r\n\r\n" +
+                                    $"Description: {reader["description"]}\r\n\r\n" +
+                                    $"Calories Per Serving: {reader["calories_per_serving"]}\r\n\r\n" +
+                                    $"Ingredients:\r\n{reader["ingredients"]}\r\n\r\n" +
+                                    $"Instructions:\r\n{reader["instructions"]}";
+                            }
+                            else
+                            {
+                                MessageBox.Show("No Jamaican recipes found in the database.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Recipes_Load(object sender, EventArgs e)
         {
 
         }
