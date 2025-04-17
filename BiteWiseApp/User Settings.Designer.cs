070a508444eddffc8174a91d52f3d7193cc527af
Kemar Lemonious <lemoniousk@gmail.com>
2025-04-16 19:04:37 -0500

    Updated names of items on Goals, set click action for goals on main menu.

--- BiteWiseApp/User Settings.Designer.cs
+++ BiteWiseApp/User Settings.Designer.cs
@@ -50,7 +50,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label5);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "User Settings";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
